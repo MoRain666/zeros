bdb015515b145645857447abd3f643070a0db38f
V.Kharitonovitch <morainrallez@gmail.com>
2018-02-22 02:38:11 +0300

    easy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count,date;
+  count = 0;
+  date = number;
+  do {
+    date = Math.floor(date / 5); 
+    count += date;
+  } while (Math.floor(date / 5) > 0);
+  return count;
 }
